b246612c0e7d32636f8b30b46811a87f3850323a
Dennis Vriend <dnvriend@gmail.com>
2016-12-10 13:52:51 +0400

    single parser test

--- src/test/scala/com/github/dnvriend/ParserTest.scala
+++ src/test/scala/com/github/dnvriend/ParserTest.scala
@@ -62,4 +62,87 @@ class ParserTest extends TestSpec {
       alwaysFail.parse("foo") should haveFailure("")
     }
   }
+
+  "sbt.complete.DefaultParsers" should "have a 'Space' parser" in {
+    //  Matches a non-empty String consisting of whitespace characters.
+    sbt.complete.DefaultParsers.Space.parse(" ") should beSuccess(Seq(' '))
+    sbt.complete.DefaultParsers.Space.parse("") should haveFailure("Expected whitespace character")
+    sbt.complete.DefaultParsers.Space.parse("abcde") should haveFailure("Expected whitespace character ...abcde")
+  }
+
+  it should "have a 'NotSpace' parser" in {
+    // Matches a non-empty String consisting of non-whitespace characters.
+    sbt.complete.DefaultParsers.NotSpace.parse("abcde") should beSuccess("abcde")
+    sbt.complete.DefaultParsers.NotSpace.parse("") should haveFailure("Expected non-whitespace character")
+    sbt.complete.DefaultParsers.NotSpace.parse("abcde abcde") should haveFailure("Expected non-whitespace character ...abcde abcde")
+  }
+
+  it should "have a 'OptSpace' parser" in {
+    //  Matches a possibly empty String consisting of whitespace characters.
+    sbt.complete.DefaultParsers.OptSpace.parse(" ") should beSuccess(Seq(' '))
+    sbt.complete.DefaultParsers.OptSpace.parse("") should beSuccess(Seq.empty[Char])
+    sbt.complete.DefaultParsers.OptSpace.parse("abcde") should haveFailure("Expected whitespace character ...abcde")
+  }
+
+  it should "have a 'OptNotSpace' parser" in {
+    // Matches a possibly empty String consisting of non-whitespace characters.
+    sbt.complete.DefaultParsers.OptNotSpace.parse(" ") should haveFailure("Expected non-whitespace character")
+    sbt.complete.DefaultParsers.OptNotSpace.parse("") should beSuccess("")
+    sbt.complete.DefaultParsers.OptNotSpace.parse("abcde") should beSuccess("abcde")
+    sbt.complete.DefaultParsers.OptNotSpace.parse("abcde abcde") should haveFailure("Expected non-whitespace character ...abcde abcde")
+  }
+
+  it should "have a 'StringBasic' parser" in {
+    // Parses a potentially quoted String value. The value may be verbatim quoted (StringVerbatim),
+    // quoted with interpreted escapes (StringEscapable), or unquoted (NotQuoted).
+    sbt.complete.DefaultParsers.StringBasic.parse("") shouldBe failure
+    sbt.complete.DefaultParsers.StringBasic.parse("'abcde'") should beSuccess("'abcde'")
+  }
+
+  it should "have a 'IntBasic' parser" in {
+    // Parses a signed integer
+    sbt.complete.DefaultParsers.IntBasic.parse("") should haveFailure("Expected digit")
+    sbt.complete.DefaultParsers.IntBasic.parse("a") should haveFailure("Expected '-' ...Expected digit ...a")
+    sbt.complete.DefaultParsers.IntBasic.parse(Long.MaxValue.toString) should haveFailure("""java.lang.NumberFormatException: For input string: "9223372036854775807" ...9223372036854775807""")
+    sbt.complete.DefaultParsers.IntBasic.parse(Long.MinValue.toString) should haveFailure("""java.lang.NumberFormatException: For input string: "-9223372036854775808" ...-9223372036854775808""")
+    sbt.complete.DefaultParsers.IntBasic.parse("0") should beSuccess(0)
+    sbt.complete.DefaultParsers.IntBasic.parse(Int.MaxValue.toString) should beSuccess(Int.MaxValue)
+    sbt.complete.DefaultParsers.IntBasic.parse(Int.MinValue.toString) should beSuccess(Int.MinValue)
+  }
+
+  it should "have a 'Digit' parser" in {
+    // Parses any single digit and provides that digit as a Char as the result
+    sbt.complete.DefaultParsers.Digit.parse("") should haveFailure("Expected digit")
+    sbt.complete.DefaultParsers.Digit.parse("-1") should haveFailure("Expected digit ...-1")
+    sbt.complete.DefaultParsers.Digit.parse("0") should beSuccess('0')
+  }
+
+  it should "have a 'HexDigit' parser" in {
+    // Parses a single hexadecimal digit (0-9, a-f, A-F)
+    sbt.complete.DefaultParsers.HexDigit.parse("") should haveFailure("Expected hex digit")
+    sbt.complete.DefaultParsers.HexDigit.parse("-1") should haveFailure("Expected hex digit ...-1")
+    sbt.complete.DefaultParsers.HexDigit.parse("F") should beSuccess('F')
+  }
+
+  it should "have a 'Bool' parser" in {
+    // Parses the lower-case values true and false into their respesct Boolean values
+    sbt.complete.DefaultParsers.Bool.parse("") should haveFailure("Expected 'true' ...Expected 'false'")
+    sbt.complete.DefaultParsers.Bool.parse("0") should haveFailure("Expected 'true' ...Expected 'false' ...0")
+    sbt.complete.DefaultParsers.Bool.parse("1") should haveFailure("Expected 'true' ...Expected 'false' ...1")
+    sbt.complete.DefaultParsers.Bool.parse("on") should haveFailure("Expected 'true' ...Expected 'false' ...on")
+    sbt.complete.DefaultParsers.Bool.parse("ON") should haveFailure("Expected 'true' ...Expected 'false' ...ON")
+    sbt.complete.DefaultParsers.Bool.parse("off") should haveFailure("Expected 'true' ...Expected 'false' ...off")
+    sbt.complete.DefaultParsers.Bool.parse("OFF") should haveFailure("Expected 'true' ...Expected 'false' ...OFF")
+    sbt.complete.DefaultParsers.Bool.parse("TRUE") should haveFailure("Expected 'true' ...Expected 'false' ...TRUE")
+    sbt.complete.DefaultParsers.Bool.parse("FALSE") should haveFailure("Expected 'true' ...Expected 'false' ...FALSE")
+    sbt.complete.DefaultParsers.Bool.parse("true") should beSuccess(true)
+    sbt.complete.DefaultParsers.Bool.parse("false") should beSuccess(false)
+  }
+
+  it should "have a 'EOF' parser" in {
+    sbt.complete.DefaultParsers.EOF.parse("") should beSuccess(())
+    sbt.complete.DefaultParsers.EOF.parse("foobar") should haveFailure("Excluded. ...foobar")
+  }
+
+  // there are more parsers in the sbt.complete.DefaultParsers object/module
 }
